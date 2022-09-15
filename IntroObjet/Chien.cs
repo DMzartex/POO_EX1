a89220b7559364b1d8871c773d210ce664feef69
DMzartex <190478@site.asty-moulin.be>
2022-09-15 22:49:19 +0200

    Ajout POO ex 1

---
+++ IntroObjet/Chien.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroObjet
+{
+    class Chien
+    {
+        private string _nom;
+        private string _race;
+        private int _age;
+
+
+        public Chien(string nom, string race, int age)
+        {
+            this._nom = nom;
+            this._race = race;
+            this._age = age;
+        }
+
+        public string AfficherCaracteristiques()
+        {
+            return "Nom : " + this._nom + " Age" + this._age + " Race : " + this._race;   
+        }
+
+        public int AfficheAge() { return this._age; }
+    }
+}
---
+++ IntroObjet/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace IntroObjet 
+{
+    class Program
+    {
+        static void insertChien(int nbrChien, out Chien[] mesChiens)
+        {
+            mesChiens = new Chien[nbrChien];
+            for (int i = 0; i < nbrChien; i++)
+            {
+                Console.WriteLine("Quel est le nom du chien ?");
+                string nomChien = Console.ReadLine();
+                Console.WriteLine("Quelle est la race ?");
+                string raceChien = Console.ReadLine();
+                Console.WriteLine("Quel l'age du chien ?");
+                int ageChien = int.Parse(Console.ReadLine());
+                mesChiens[i] = new Chien(nomChien, raceChien, ageChien);
+            }
+
+        }
+
+        static void afficheChien(Chien[] mesChiens)
+        {
+            for (int i = 0; i < mesChiens.Length; i++)
+            {
+                Console.WriteLine(mesChiens[i].AfficherCaracteristiques());
+            }
+        }
+
+        static void moyenneAge(int nbrChien, Chien[] mesChiens, out int resultMoyenne)
+        {
+            resultMoyenne = 0;
+            int totalAge = 0;
+            for(int i = 0; i < mesChiens.Length; i++)
+            {
+                totalAge = totalAge + mesChiens[i].AfficheAge();
+            }
+            resultMoyenne = totalAge / nbrChien;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenu dans notre chenil !");
+            int nbrChien = 0;
+            int agetotal = 0;
+            int resultMoyenne;
+            Chien[] mesChiens;
+            Console.WriteLine("Combien de chiens voulez vous insérer ?");
+            nbrChien = int.Parse(Console.ReadLine());
+
+            insertChien(nbrChien, out mesChiens);
+            afficheChien(mesChiens);
+            moyenneAge(nbrChien,mesChiens,out resultMoyenne);
+            Console.WriteLine("La moyenne d'age des chiens est de :" + resultMoyenne);
+
+            
+        }
+    }
+}
